7d73993b823841cbaa7961d0e5a51d34aef6bc8e
mvrk <oscarmenacho23@gmail.com>
2022-04-29 06:25:58 -0500

    useFetch + useCounter

--- src/components/03-examples/MultipleCustomHooks.jsx
+++ src/components/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import '../02-useEffect/effects.css'
-import { useFetch } from '../hooks/useFetch'
+import React from "react";
+import "../02-useEffect/effects.css";
+import { useCounter } from "../hooks/useCounter";
+import { useFetch } from "../hooks/useFetch";
 
 export const MultipleCustomHooks = () => {
+  const { counter, increment } = useCounter(1);
 
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+  const { loading, data } = useFetch(
+    `https://www.breakingbadapi.com/api/quotes/${counter}`
+  );
+
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
-        <h1>Custom Hooks!!!</h1>
+      <h1>BreakingBad Quotes</h1>
+      <hr />
+
+      {loading ? (
+        <div className="alert alert-info text-center">Loading...</div>
+      ) : (
+        <blockquote className="blockquote text-right">
+          <p className="mb-0"> {quote}</p>
+          <footer className="blockquote-footer"> {author} </footer>
+        </blockquote>
+      )}
+
+      <button className="btn btn-primary" onClick={increment}>
+        Siguiente quote
+      </button>
     </>
-  )
-}
+  );
+};
--- src/components/hooks/useCounter.js
+++ src/components/hooks/useCounter.js
@@ -2,22 +2,22 @@ import { useState } from "react"
 
 export const useCounter = ( initialState = 10 ) => {
   
-  const [state, setState] = useState( initialState );
+  const [counter, setCounter] = useState( initialState );
 
   const reset = () => {
-    setState( initialState )
+    setCounter( initialState )
   }
 
-  const increment = ( factor = 1 ) => {
-    setState( state + factor );
+  const increment = () => {
+    setCounter( counter + 1 );
   }
 
-  const decrement = ( factor = 1 ) => {
-    setState( state - factor );
+  const decrement = () => {
+    setCounter( counter - 1 );
   }
 
   return {
-    state,
+    counter,
     increment,
     decrement,
     reset
