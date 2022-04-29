52798cc51ee33256e70ed2387415af8baa2e465e
mvrk <oscarmenacho23@gmail.com>
2022-04-29 05:17:49 -0500

    Formulario con custom Hook

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { SimpleForm } from './components/02-useEffect/SimpleForm'
+import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
+// import { SimpleForm } from './components/02-useEffect/SimpleForm'
 // import { CounterWithCustomHooks } from './components/01-useState/CounterWithCustomHooks'
 // import { CounterApp } from './components/01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
@@ -8,6 +9,6 @@ import { SimpleForm } from './components/02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <SimpleForm />
+    <FormWithCustomHook />
   </>
 )
