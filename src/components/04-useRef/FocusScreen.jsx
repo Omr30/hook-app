6389f9d36d1e898cb848cfd12dde0ab58b9039d9
mvrk <oscarmenacho23@gmail.com>
2022-05-10 00:27:34 -0500

    useRef - Primer Uso

---
+++ src/components/04-useRef/FocusScreen.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react'
+import '../02-useEffect/effects.css'
+
+export const FocusScreen = () => {
+
+    const inputRef = useRef();
+    // console.log(ref)
+
+    const handleClick = () => {
+        inputRef.current.select();
+    }
+
+  return (
+    <>
+        <h1>FocusScreen</h1>
+        <hr />
+
+        <input
+            ref={ inputRef } 
+            className='form-control'
+            placeholder='Su nombre' 
+        />
+
+        <button 
+            className='btn btn-outline-primary mt-5'
+            onClick={ handleClick }
+        >
+            Focus
+        </button>
+    </>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
+import { FocusScreen } from './components/04-useRef/FocusScreen'
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks'
 // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook'
 // import { SimpleForm } from './components/02-useEffect/SimpleForm'
 // import { CounterWithCustomHooks } from './components/01-useState/CounterWithCustomHooks'
@@ -10,6 +11,6 @@ import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHook
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <MultipleCustomHooks />
+    <FocusScreen />
   </>
 )
