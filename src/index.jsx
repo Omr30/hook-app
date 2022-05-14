f1c03aa3b67727b3d485f1cf789ae7b61d616546
mvrk <oscarmenacho23@gmail.com>
2022-05-14 12:29:16 -0500

    Tarea Memorize

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { CallbackHooks } from './components/06-memos/CallbackHooks'
+import { Padre } from './components/07-tarea-memo/Padre'
+// import { CallbackHooks } from './components/06-memos/CallbackHooks'
 // import { MemomHook } from './components/06-memos/MemoHook'
 // import { Memorize } from './components/06-memos/Memorize'
 // import { Layout } from './components/05-useLayoutEffect/Layout'
@@ -16,6 +17,6 @@ import { CallbackHooks } from './components/06-memos/CallbackHooks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <CallbackHooks />
+    <Padre />
   </>
 )
