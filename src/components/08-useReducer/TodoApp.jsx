7d045eab8310ba026dcab7e43ce33527e890ffb0
mvrk <oscarmenacho23@gmail.com>
2022-05-16 06:48:17 -0500

    Creando el cascarón de la lista de TODOs

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,32 +1,64 @@
-import React, { useReducer } from 'react'
-import { todoReducer } from './todoReducer';
+import React, { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
 
-
-import './styles.css'
+import "./styles.css";
 
 export const TodoApp = () => {
+  const initialState = [
+    {
+      id: new Date().getTime(),
+      desc: "Aprender React",
+      done: false,
+    },
+  ];
 
-    const initialState = [{
-        id: new Date().getTime(),
-        desc: 'Aprender React',
-        done: false
-    }];
+  const [todos] = useReducer(todoReducer, initialState);
 
-    const [ todos ] = useReducer(todoReducer, initialState)
-
-    console.log(todos)
+  console.log(todos);
 
   return (
     <>
-        <h1>TodoApp</h1>
-        <hr />
+      <h1>TodoApp ( {todos.length} ) </h1>
+      <hr />
 
-        <ul>
-            <li>Hola</li>
-            <li>mundo</li>
-            <li>Hola de nuevo</li>
-        </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {" "}
+                  {i + 1}. {todo.desc}{" "}
+                </p>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
 
+        <div className="col-5">
+            <h4>Agregar TODO</h4>
+            <hr />
+
+            <form action="">
+                <input 
+                    type="text"
+                    name="description"
+                    className="form-control"
+                    placeholder="Aprender ..."
+                    autoComplete="off"
+                />
+
+                <button 
+                    className="btn btn-outline-primary mt-1 w-100"
+                >
+                    Agregar
+                </button>
+
+            </form>
+
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
