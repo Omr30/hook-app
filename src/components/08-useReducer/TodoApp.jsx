65efb3d0ecc300200e99fdebd18be379a6f94303
mvrk <oscarmenacho23@gmail.com>
2022-05-19 23:56:16 -0500

    Optimizacion #1
    Listado de TODOs

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -3,58 +3,43 @@ import { todoReducer } from "./todoReducer";
 import { useForm } from "../hooks/useForm";
 
 import "./styles.css";
+import { TodoList } from "./todoList";
 
 const init = () => {
-  
-  return JSON.parse(localStorage.getItem('todos')) || [];
-  
-  // return [
-  //   {
-  //     id: new Date().getTime(),
-  //     desc: "Aprender React",
-  //     done: false,
-  //   },
-  // ];
-}
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
 
 export const TodoApp = () => {
+  const [todos, dispatch] = useReducer(todoReducer, [], init);
 
-  const [ todos, dispatch ] = useReducer(todoReducer, [], init);
-
-  const [ { description }, handleInputChange, reset ] = useForm({
-    description: ''
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
   });
 
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-  
-  }, [todos])
-  
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const handleDelete = (todoId) => {
-
-    // crear la accion
     const action = {
-      type: 'delete',
-      payload: todoId
-    }
+      type: "delete",
+      payload: todoId,
+    };
 
-    // hacer el dispatch
     dispatch(action);
   };
 
   const handleToggle = (todoId) => {
-
     dispatch({
-      type: 'toggle',
-      payload: todoId
-    })
-
-  }
+      type: "toggle",
+      payload: todoId,
+    });
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if ( description.trim().length <= 1 ) {
+    if (description.trim().length <= 1) {
       return;
     }
 
@@ -65,13 +50,13 @@ export const TodoApp = () => {
     };
 
     const action = {
-      type: 'add',
+      type: "add",
       payload: newTodo,
-    }
+    };
 
     dispatch(action);
     reset();
-  }
+  };
 
   return (
     <>
@@ -80,54 +65,35 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <ul className="list-group list-group-flush">
-            {todos.map((todo, i) => (
-              <li 
-                key={todo.id} 
-                className="list-group-item"
-              >
-                <p 
-                  className={`${ todo.done && 'complete' }`}
-                  onClick={() => handleToggle(todo.id)}
-                >
-                  {" "}
-                  {i + 1}. {todo.desc}{" "}
-                </p>
-                <button 
-                  className="btn btn-danger"
-                  onClick={() => handleDelete(todo.id)}
-                >
-                  Borrar
-                </button>
-              </li>
-            ))}
-          </ul>
+          <TodoList 
+            todos={todos}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+          />
         </div>
 
         <div className="col-5">
-            <h4>Agregar TODO</h4>
-            <hr />
+          <h4>Agregar TODO</h4>
+          <hr />
 
-            <form onSubmit={ handleSubmit }>
-                <input 
-                    type="text"
-                    name="description"
-                    className="form-control"
-                    placeholder="Aprender ..."
-                    autoComplete="off"
-                    value={ description }
-                    onChange={ handleInputChange}
-                />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              name="description"
+              className="form-control"
+              placeholder="Aprender ..."
+              autoComplete="off"
+              value={description}
+              onChange={handleInputChange}
+            />
 
-                <button
-                    type="submit" 
-                    className="btn btn-outline-primary mt-1 w-100"
-                >
-                    Agregar
-                </button>
-
-            </form>
-
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1 w-100"
+            >
+              Agregar
+            </button>
+          </form>
         </div>
       </div>
     </>
