f1a9b0e5e5e7f464a2c66672bdab112412be6f71
mvrk <oscarmenacho23@gmail.com>
2022-05-20 00:20:25 -0500

    Optimizacion #2
    Agregar TODO

--- src/components/08-useReducer/TodoApp.jsx
+++ src/components/08-useReducer/TodoApp.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
-import { useForm } from "../hooks/useForm";
+import { TodoList } from "./todoList";
+import { TodoAdd } from "./TodoAdd";
 
 import "./styles.css";
-import { TodoList } from "./todoList";
 
 const init = () => {
   return JSON.parse(localStorage.getItem("todos")) || [];
@@ -11,10 +11,6 @@ const init = () => {
 
 export const TodoApp = () => {
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: "",
-  });
 
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
@@ -36,27 +32,14 @@ export const TodoApp = () => {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (description.trim().length <= 1) {
-      return;
-    }
-
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
-
-    const action = {
+  const handleAddTodo = ( newTodo ) => {
+    dispatch({
       type: "add",
       payload: newTodo,
-    };
+    });
+  }
+  
 
-    dispatch(action);
-    reset();
-  };
 
   return (
     <>
@@ -73,27 +56,9 @@ export const TodoApp = () => {
         </div>
 
         <div className="col-5">
-          <h4>Agregar TODO</h4>
-          <hr />
-
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="description"
-              className="form-control"
-              placeholder="Aprender ..."
-              autoComplete="off"
-              value={description}
-              onChange={handleInputChange}
-            />
-
-            <button
-              type="submit"
-              className="btn btn-outline-primary mt-1 w-100"
-            >
-              Agregar
-            </button>
-          </form>
+          <TodoAdd 
+            handleAddTodo={handleAddTodo}
+          />
         </div>
       </div>
     </>
