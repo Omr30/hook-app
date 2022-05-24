83bfb4e1a3282f114b8b010f182520234814ed9a
mvrk <oscarmenacho23@gmail.com>
2022-05-24 14:18:11 -0500

    CreateContext y ContextProvider

--- src/components/09-useContext/HomeScreen.jsx
+++ src/components/09-useContext/HomeScreen.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from './UserContext';
 
 export const HomeScreen = () => {
+
+  const userContext = useContext(UserContext);
+  
+  console.log(userContext);
+
   return (
     <>
         <h1>HomeScreen</h1>
--- src/components/09-useContext/MainApp.jsx
+++ src/components/09-useContext/MainApp.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
-import { AppRouter } from './AppRouter'
+import React from "react";
+import { AppRouter } from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 export const MainApp = () => {
+
+  const user = {
+    id: 1234,
+    name: 'Mvrk',
+    email: 'mvrk@mvrk.com'
+  }
+
   return (
-        <AppRouter />
-  )
-}
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
+};
